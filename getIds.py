7e48fd430f11a16b213dd51257806f07dff758d6
cooga011 <cooga011@umn.edu>
2017-03-10 08:33:22 -0600

    cleanup, incorporate prm file

--- getIds.py
+++ getIds.py
@@ -81,6 +81,8 @@ def main():
     DriveService = discovery.build('drive', 'v3', http=http)
 
 
+
+	# Pull spreadsheet data from Google Drive (incorporate parse args replace q="name = 'SX_RunLog'+ with just initials
     DriveResults = DriveService.files().list(q="name = 'SX_RunLog'", pageSize=50, fields="nextPageToken, files(id, name)").execute()
     items = DriveResults.get('files', [])
     if not items:
@@ -92,21 +94,26 @@ def main():
             print("Name: " + item['name'])
             print("ID: " + item['id'])
 
+
+	# Set up variables to be used to post to Google Sheets and scrape the applog
     spreadsheetId = [item['id']]
     subsheet = ['Summary','Pre-MBSR EEG', 'Pre-BMSR BCI', 'BCI 1','BCI 2','BCI 3','BCI 4','BCI 5','BCI 6','Post-BCI EEG']
     BCIsubSheet = [subsheet[2:8]]
 #    for sheets in range(0,7):
         dataFilename.append([subsheet[sheets]+'!C9',subsheet[sheets]+'!C12',subsheet[sheets]+'!C15',subsheet[sheets]+'!C19',subsheet[sheets]+'!C22',subsheet[sheets]+'!C25'])  #For BCI 1 label
-
-    hitRange = 'BCI 1!F9:F27'
-    invalidRange = 'BCI 1!G9:G27'
+        hitRange = subsheet[sheets]+'!F9:F27'
+        invalidRange = subsheet[sheets]+'!G9:G27'
+        prmLoc = [subsheet[sheets]+'!D11',subsheet[sheets]+'D!14', subsheet[sheets]+'D!17',subsheet[sheets]+ 'D!21', subsheet[sheets]+'D!24', subsheet[sheets]+'D!27']
+    
     tasks = ["LR1","UD1","2D1","LR2","UD2","2D2"]
     endOfLine = "S001.applog"
 
-    filename = SpreadsheetService.spreadsheets().values().batchGet(
-        spreadsheetId=spreadsheetId[0], ranges=dataFilename).execute()
+
+	#Pull data from Google Sheet
+    filename = SpreadsheetService.spreadsheets().values().batchGet(spreadsheetId=spreadsheetId[0], ranges=dataFilename).execute()
     filenameParams = filename.get('valueRanges')
-    for vals in range(0,5):
+
+    for vals in range(0,5):														#edit s oit can take ranges from all the sheets, not just 'BCI 1...
         fileNameValues.append(filenameParams[vals].get('values'))
     subjectInits = fileNameValues[1][0][0][0:4]
     dateOfExp = fileNameValues[1][0][0][7:16]
@@ -116,7 +123,7 @@ def main():
 
     #Read data from applog
     for taskNumber in range(0,6):
-        fileToRead = "Z:\EEGDATA\Scraper\SX"+"/"+subjectInits+tasks[taskNumber]+dateOfExp+endOfLine[1:4]+"/"+subjectInits+tasks[taskNumber]+dateOfExp+endOfLine   #modify directory based on what computer this is on
+        fileToRead = "Z:\EEGDATA\Scraper\SX"+"/"+subjectInits+tasks[taskNumber]+dateOfExp+endOfLine[1:4]+"/"+subjectInits+tasks[taskNumber]+dateOfExp+endOfLine   #modify directory based on what computer 																																										this is on
         with open(fileToRead,'r') as f:
             for line in f:
                 for word in line.split():
@@ -128,10 +135,16 @@ def main():
         f.close()
 
     #Get values to write to spreadsheet
+    #for valzz in range(0,17):
+        #hitValues.append(hits[valzz])
+        #invalidValues.append(invalid[valzz])				Set up so that we skip one spot in between 8 and 9 (2D1 and LR2)
+
+
     hitValues = [[hits[0]],[hits[1]],[hits[2]],[hits[3]],[hits[4]],[hits[5]],[hits[6]],[hits[7]], [hits[8]],[],\
         [hits[9]],[hits[10]],[hits[11]],[hits[12]],[hits[13]],[hits[14]],[hits[15]],[hits[16]],[hits[17]]]
     invalidValues = [[invalid[0]],[invalid[1]],[invalid[2]],[invalid[3]],[invalid[4]],[invalid[5]],[invalid[6]],[invalid[7]],[invalid[8]],[],\
         [invalid[9]],[invalid[10]],[invalid[11]],[invalid[12]],[invalid[13]],[invalid[14]],[invalid[15]],[invalid[16]],[invalid[17]]]
+
     data = [
     	{
     		'range': hitRange,
